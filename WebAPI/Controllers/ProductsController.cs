80827bb0186a9d6d980fa3fad36525cf5171c842
Sueda Elaman <suedaelaman@gmail.com>
2022-08-09 17:14:02 +0300

    09.08.2022 1. commit

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Loosely Coupled
+        //naming convention
+        //IoC Container Inversion of Control
+
+        IProductService _productService; //IProductService türünde field 
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            //Dependency Chain
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+
+
+    }
+
+
+}
+
+
+
