521f7402c388485ab21325300fb35117479afae2
Sueda Elaman <suedaelaman@gmail.com>
2022-08-10 13:24:52 +0300

    10.08.2022 2.commit

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -42,11 +42,11 @@ namespace Business.Concrete
         {
             //iş kodları
 
-            //if (DateTime.Now.Hour == 22)
-            //{
-            //    return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            if (DateTime.Now.Hour == 1)
+            {
+                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
 
-            //}
+            }
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
 
         }
